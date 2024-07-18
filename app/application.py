4e2be43fabe7c8ae699d745e90c8fb00a56cf15c
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2024-07-18 14:38:05 +0200

    Change path for sqlite3 connection

--- app/application.py
+++ app/application.py
@@ -24,9 +24,9 @@ Prevention:
 app.secret_key = os.getenv("LOG_KEY")
 app.logger.setLevel(logging.INFO)
 
-
+# Use relocated .db file
 def get_db_connection():
-    connection = sqlite3.connect("database.db")
+    connection = sqlite3.connect("../database.db")
     connection.row_factory = sqlite3.Row
     return connection
 
